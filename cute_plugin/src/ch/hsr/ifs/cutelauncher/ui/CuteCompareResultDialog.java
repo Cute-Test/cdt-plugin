7dd0a73b2b5cd69ecc625b70f45dfc22c3b21da3
Emanuel Graf <emanuel@graf.name>
2007-02-07 18:00:51 +0000

    Testresultat-Vergleichdialog erstellt.

--- cute_plugin/src/ch/hsr/ifs/cutelauncher/ui/CuteCompareResultDialog.java
+++ cute_plugin/src/ch/hsr/ifs/cutelauncher/ui/CuteCompareResultDialog.java
@@ -20,15 +20,18 @@ import org.eclipse.compare.ITypedElement;
 import org.eclipse.compare.contentmergeviewer.TextMergeViewer;
 import org.eclipse.compare.structuremergeviewer.DiffNode;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.TrayDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
 
 import ch.hsr.ifs.cutelauncher.model.TestCase;
-import ch.hsr.ifs.cutelauncher.model.TestElement;
 
 /**
  * @author Emanuel Graf
@@ -67,32 +70,67 @@ public class CuteCompareResultDialog extends TrayDialog {
 	private TextMergeViewer compareViewer;
     private String expected = "expected";
     private String actual = "actual";
-    private TestCase test;
+    TestCase test;
 
 	public CuteCompareResultDialog(Shell shell, TestCase test) {
 		super(shell);
-		setShellStyle(SWT.DIALOG_TRIM | SWT.RESIZE | SWT.MAX);
 		this.test = test;
+		setHelpAvailable(false);
+		setShellStyle(SWT.DIALOG_TRIM | SWT.RESIZE | SWT.MAX);
+		
 	}
 
 	@Override
 	protected Control createDialogArea(Composite parent) {
+		Composite composite = (Composite) super.createDialogArea(parent);
+		ComparePane pane = new ComparePane(composite, SWT.BORDER | SWT.FLAT);
+		GridData data= new GridData(GridData.FILL_HORIZONTAL | GridData.FILL_VERTICAL);
+		data.widthHint= convertWidthInCharsToPixels(120);
+		data.heightHint= convertHeightInCharsToPixels(13);
+		pane.setLayoutData(data);
+		Control control = createCompareViewer(pane);
+		pane.setContent(control);
+		GridData gd= new GridData(GridData.FILL_BOTH);
+		control.setLayoutData(gd);
+		return composite;
+	}
+	
+	private Control createCompareViewer(ComparePane pane) {
 		final CompareConfiguration compareConfiguration= new CompareConfiguration();
-		compareConfiguration.setLeftEditable(false);
-		compareConfiguration.setLeftLabel("Expected");
-		compareConfiguration.setRightEditable(false);
-		compareConfiguration.setRightLabel("Actual");
-		compareViewer = new TextMergeViewer(parent, compareConfiguration);
-		setCompareViewerInput(test);
-		return compareViewer.getControl();
+	    compareConfiguration.setLeftLabel("Expected"); 
+	    compareConfiguration.setLeftEditable(false);
+	    compareConfiguration.setRightLabel("Actual");	 
+	    compareConfiguration.setRightEditable(false);
+	    compareConfiguration.setProperty(CompareConfiguration.IGNORE_WHITESPACE, Boolean.FALSE);
+
+	    compareViewer = new TextMergeViewer(pane, SWT.NONE, compareConfiguration);
+	    setCompareViewerInput(test);
+
+	    Control control= compareViewer.getControl();
+	    control.addDisposeListener(new DisposeListener() {
+	        public void widgetDisposed(DisposeEvent e) {
+                compareConfiguration.dispose();
+	        }
+	    });
+	    return control;
+		
+	}
+
+	protected void createButtonsForButtonBar(Composite parent) {
+		createButton(parent, IDialogConstants.OK_ID, "OK", true); 
 	}
 	
 	public void setCompareViewerInput(TestCase test) {
-		if (! compareViewer.getControl().isDisposed())
+		this.test = test;
+		if (! compareViewer.getControl().isDisposed()) {
 			compareViewer.setInput(new DiffNode(new CompareElement(expected), new CompareElement(actual)));
+		}
 	}
 	
-	
+    protected void configureShell(Shell newShell) {
+		super.configureShell(newShell);
+		newShell.setText("Result Comparison");
+	}
 
 	
 }
