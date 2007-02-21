10af1b3ecc273398cde03ee2fbcfec82b0c37250
Emanuel Graf <emanuel@graf.name>
2007-02-21 14:29:39 +0000

    neue Version ist in den falschen Ordner kopiert worden.

--- cute/cute_lib/eclipse_listener.h
+++ cute/cute_lib/eclipse_listener.h
@@ -2,30 +2,44 @@
 #define ECLIPSE_LISTENER_H_
 #include "ostream_listener.h"
 #include <iostream>
+#include <iterator>
+#include <algorithm>
 namespace cute {
 
 	class eclipse_listener 
 	{
+	protected:
+		struct blankToUnderscore{
+            char operator()(char in){
+			if (in == ' ') return '_';
+			return in;
+		}
+        };
+		std::string maskBlanks(const std::string &in) {
+			std::string result;
+			std::transform(in.begin(),in.end(),std::back_inserter(result),blankToUnderscore());
+			return result;
+		}
 	public:
 		eclipse_listener() {}
 		void start(test const &t){
-			std::cout << "#starting\x1F" <<t.name()<< std::endl;
+			std::cout << "#starting " <<t.name()<< std::endl;
 		}
 		
 		void begin(suite const &t,char const *info){
-			std::cout << "#beginning\x1F" << info << "\x1F" << t.size() << std::endl;
+			std::cout << "#beginning " << info << " " << t.size() << std::endl;
 		}
 		void end(suite const &t, char const *info){
-			std::cout << "#ending\x1F" << info << std::endl;
+			std::cout << "#ending " << info << std::endl;
 		}
 		void success(test const &t, char const *msg){
-			std::cout << "#success\x1F" <<  t.name() <<"\x1F" << msg<< std::endl;
+			std::cout << "#success " <<  maskBlanks(t.name()) <<" " << msg<< std::endl;
 		}
 		void failure(test const &t,test_failure const &e){
-			std::cout << "#failure\x1F" << e.filename << "\x1F" << e.lineno << "\x1F" <<e.reason << "§" << t.name()<< std::endl;
+			std::cout << "#failure " << maskBlanks(t.name()) << " " << e.filename << ":" << e.lineno << " " <<e.reason << std::endl;
 		}
 		void error(test const &t, char const *what){
-			std::cout << "#error\x1F" << what << "\x1F" << t.name() << std::endl;
+			std::cout << "#error " << maskBlanks(t.name()) << " " << what << std::endl;
 		}
 	};
 }
