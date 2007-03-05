b0353bad3926cc6ec6f3b3e716a49112a951acb0
Emanuel Graf <emanuel@graf.name>
2007-03-05 15:40:06 +0000

    test die ein ui benötigen auskommentiert.

--- cute_test_plugin/src/ch/hsr/ifs/cutelauncher/test/AllTests.java
+++ cute_test_plugin/src/ch/hsr/ifs/cutelauncher/test/AllTests.java
@@ -1,19 +1,18 @@
 package ch.hsr.ifs.cutelauncher.test;
 
-import ch.hsr.ifs.cutelauncher.test.hyperlinksTests.HyperlinkSuite;
-import ch.hsr.ifs.cutelauncher.test.modelBuilderTests.ModelBuilderSuite;
-import ch.hsr.ifs.cutelauncher.test.patternListenerTests.PatternListenerSuite;
 import junit.framework.Test;
 import junit.framework.TestSuite;
+import ch.hsr.ifs.cutelauncher.test.modelBuilderTests.ModelBuilderSuite;
+import ch.hsr.ifs.cutelauncher.test.patternListenerTests.PatternListenerSuite;
 
 public class AllTests {
 
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Cute Plugin All Tests");
 		//$JUnit-BEGIN$
-//		suite.addTest(PatternListenerSuite.suite());
-		suite.addTest(HyperlinkSuite.suite());
-//		suite.addTest(ModelBuilderSuite.suite());
+		suite.addTest(PatternListenerSuite.suite());
+//		suite.addTest(HyperlinkSuite.suite()); // TODO need ui-test
+		suite.addTest(ModelBuilderSuite.suite());
 		//$JUnit-END$
 		return suite;
 	}
