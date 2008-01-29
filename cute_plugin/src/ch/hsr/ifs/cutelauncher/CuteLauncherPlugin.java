a52c9feef1f979215606609ae4b00984fab8a62c
Emanuel Graf <emanuel@graf.name>
2008-01-29 16:40:32 +0000

    deleting javadoc

--- cute_plugin/src/ch/hsr/ifs/cutelauncher/CuteLauncherPlugin.java
+++ cute_plugin/src/ch/hsr/ifs/cutelauncher/CuteLauncherPlugin.java
@@ -53,20 +53,12 @@ public class CuteLauncherPlugin extends AbstractUIPlugin {
 		plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
 	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		display = Display.getCurrent();
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
 	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
