abfff2e713d520cce977bafd0a7f6dd7e27b65c8
james <james>
2008-02-01 18:26:29 +0000

    Cute Suite Wizard UI component

--- cute_plugin/src/ch/hsr/ifs/cutelauncher/ui/CuteSuiteWizardHandler.java
+++ cute_plugin/src/ch/hsr/ifs/cutelauncher/ui/CuteSuiteWizardHandler.java
@@ -18,6 +18,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Enumeration;
 
+import org.eclipse.cdt.managedbuilder.ui.wizards.MBSCustomPageManager;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IResource;
@@ -27,18 +28,31 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubProgressMonitor;
 import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.swt.widgets.Composite;
 
 import ch.hsr.ifs.cutelauncher.CuteLauncherPlugin;
 public class CuteSuiteWizardHandler extends CuteWizardHandler {
+	private final NewCuteSuiteWizardCustomPage libRefPage;
 	public CuteSuiteWizardHandler(Composite p, IWizard w) {
 		super( p, w);
+		libRefPage = new NewCuteSuiteWizardCustomPage(getConfigPage(), getStartingPage());
+		libRefPage.setPreviousPage(getStartingPage());
+		libRefPage.setWizard(getWizard());
+		MBSCustomPageManager.init();
+		MBSCustomPageManager.addStockPage(libRefPage, libRefPage.getPageID());
+	}
+	@Override
+	public IWizardPage getSpecificPage() {
+		
+		return libRefPage;
 	}
 	
-	String suitename="someHowSuite";
+	String suitename;
 	
 	@Override
 	public void addTestFiles(IFolder folder, IProgressMonitor monitor) throws CoreException {
+		suitename=libRefPage.getSuiteName();
 		copyFile(folder,monitor,"Test.cpp","Test.cpp");
 		copyFile(folder,monitor,"$suitename$.cpp",suitename+".cpp");
 		copyFile(folder,monitor,"$suitename$.h",suitename+".h");
---
+++ cute_plugin/src/ch/hsr/ifs/cutelauncher/ui/NewCuteSuiteWizardCustomPage.java
@@ -0,0 +1,142 @@
+package ch.hsr.ifs.cutelauncher.ui;
+
+import org.eclipse.cdt.managedbuilder.ui.wizards.CDTConfigWizardPage;
+import org.eclipse.cdt.managedbuilder.ui.wizards.MBSCustomPage;
+import org.eclipse.debug.internal.ui.SWTFactory;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.jface.wizard.IWizardContainer;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+public class NewCuteSuiteWizardCustomPage extends MBSCustomPage {
+
+	private final CDTConfigWizardPage conifgPage;
+	private final IWizardPage startingWizardPage;
+	
+	public NewCuteSuiteWizardCustomPage(CDTConfigWizardPage configWizardPage, IWizardPage staringWizardPage){
+		pageID="ch.hsr.ifs.cutelauncher.ui.NewCuteSuiteWizardCustomPage";
+		this.conifgPage = configWizardPage;
+		this.startingWizardPage = staringWizardPage;
+	}
+
+	@Override
+	public IWizardPage getNextPage() {
+		return conifgPage;
+	}
+
+	@Override
+	public IWizardPage getPreviousPage() {
+		return startingWizardPage;
+	}
+	
+	@Override
+	protected boolean isCustomPageComplete() {
+		if(suitenameText.getText().equals("")){ 
+			errmsg="Please enter a suite name.";
+			return false;
+		}
+		errmsg=null;
+		return true;
+	}
+
+	public String getName() {
+		return "Set Suite Name";
+	}
+	
+	private Composite composite=null;
+	private Label suitenameLabel=null; 
+	private Text suitenameText=null;
+	
+	public void createControl(Composite parent) {
+		//descriptionLabel=new Label(n_comp,SWT.NONE);
+	    //descriptionLabel.setText("This is for unmanaged project to set the source folder for Cute Plug-in to scan.");
+		composite = new Composite(parent, SWT.NULL);
+		
+		GridLayout layout = new GridLayout(3, true);
+		//layout.numColumns = 2;
+		layout.marginHeight = 0;
+		layout.marginWidth = 5;
+		composite.setLayout(layout);
+		composite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
+		
+		GridData gd;
+		gd = new GridData();
+		gd.horizontalSpan =1;
+		suitenameLabel=new Label(composite,SWT.NONE);
+		suitenameLabel.setText("Test Suite Name:");
+		suitenameLabel.setLayoutData(gd);
+		
+	    gd = new GridData(GridData.FILL_HORIZONTAL);
+		gd.horizontalSpan =2;
+		suitenameText = SWTFactory.createSingleText(composite, 2);
+		suitenameText.setLayoutData(gd); 
+		suitenameText.setText("suite");
+	    suitenameText.addModifyListener(new ModifyListener() {
+	    	public void modifyText(ModifyEvent e){
+			IWizardContainer iwc=getWizard().getContainer();
+			iwc.updateButtons();
+			iwc.updateMessage();
+			iwc.updateTitleBar();
+			iwc.updateWindowTitle();}
+		});	
+	    //no access to canFinish() so have to use default values instead 
+	}
+
+	public String getSuiteName(){
+		if(suitenameText.getText().equals(""))return "suite";
+		return suitenameText.getText();
+	}
+	public void dispose() {
+		composite.dispose();
+	}
+
+	public Control getControl() {
+		return composite==null?null:composite;
+	}
+
+	public String getDescription() {
+		return "for the user to specify a custom suite name.";
+	}
+
+	String errmsg=null;
+	public String getErrorMessage() {
+		return errmsg;
+	}
+
+	public Image getImage() {
+		return wizard.getDefaultPageImage();
+	}
+
+	public String getMessage() {
+		return "New Test Suite Name";
+	}
+
+	public String getTitle() {
+		return "Suite Name";
+	}
+
+	public void performHelp() {
+	}
+
+	public void setDescription(String description) {
+	}
+
+	public void setImageDescriptor(ImageDescriptor image) {
+	}
+
+	public void setTitle(String title) {
+	}
+
+	public void setVisible(boolean visible) {
+		composite.setVisible(visible);
+	}
+
+}
