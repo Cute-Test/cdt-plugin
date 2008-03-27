b6b1d66137f623b3a984ae87fe4d6db590b99de5
james <james>
2008-03-27 17:43:25 +0000

    addtestfunction pushback detection

--- cute_plugin/src/ch/hsr/ifs/cutelauncher/ui/sourceactions/AddTestFunctiontoSuiteAction.java
+++ cute_plugin/src/ch/hsr/ifs/cutelauncher/ui/sourceactions/AddTestFunctiontoSuiteAction.java
@@ -1,13 +1,22 @@
 package ch.hsr.ifs.cutelauncher.ui.sourceactions;
 
 import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
+import org.eclipse.cdt.core.dom.ast.IASTExpression;
+import org.eclipse.cdt.core.dom.ast.IASTExpressionList;
+import org.eclipse.cdt.core.dom.ast.IASTFieldReference;
+import org.eclipse.cdt.core.dom.ast.IASTFunctionCallExpression;
 import org.eclipse.cdt.core.dom.ast.IASTFunctionDefinition;
 import org.eclipse.cdt.core.dom.ast.IASTName;
 import org.eclipse.cdt.core.dom.ast.IASTNode;
 import org.eclipse.cdt.core.dom.ast.IASTParameterDeclaration;
 import org.eclipse.cdt.core.dom.ast.IASTSimpleDeclSpecifier;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.dom.ast.IBinding;
+import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTFieldReference;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTFunctionDeclarator;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTFieldReference;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTFunctionCallExpression;
+import org.eclipse.cdt.internal.core.dom.parser.cpp.CPPASTIdExpression;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.IDocument;
@@ -38,7 +47,8 @@ public class AddTestFunctiontoSuiteAction extends AbstractFunctionAction{
 				astTu.accept(suitPushBackFinder);
 
 				String fname=getFunctionNameAtCursor(astTu, selection,suitPushBackFinder);
-				if(!dontAddFlag && !checkNameExist(astTu,fname,suitPushBackFinder))
+				//if(!dontAddFlag && !checkNameExist(astTu,fname,suitPushBackFinder))
+				if(!dontAddFlag && !checkPushback(astTu,fname,suitPushBackFinder))
 				{
 					mEdit.addChild(createPushBackEdit(editorFile, doc, astTu,
 						fname, suitPushBackFinder));
@@ -94,6 +104,54 @@ public class AddTestFunctiontoSuiteAction extends AbstractFunctionAction{
 		}
 		return null;
 	}
-
+	
+	@SuppressWarnings("restriction")
+	private boolean checkPushback(IASTTranslationUnit astTu,String fname,SuitePushBackFinder suitPushBackFinder){
+		if(suitPushBackFinder.getSuiteDeclName() != null) {
+			IASTName name = suitPushBackFinder.getSuiteDeclName();
+			IBinding binding = name.resolveBinding();
+			IASTName[] refs = astTu.getReferences(binding);
+			for (IASTName name1 : refs) {
+				try{
+					IASTFieldReference fRef = (ICPPASTFieldReference) name1.getParent().getParent();
+					if(fRef.getFieldName().toString().equals("push_back")) {
+						IASTFunctionCallExpression callex=(IASTFunctionCallExpression)name1.getParent().getParent().getParent();
+						IASTFunctionCallExpression innercallex=(IASTFunctionCallExpression)callex.getParameterExpression();
+						IASTExpression thelist=innercallex.getParameterExpression();
+						String theName="";
+						if(thelist!=null){
+							if(thelist instanceof IASTExpressionList){
+							}else{//for 3rd test case ***already added***
+								theName=((CPPASTIdExpression)thelist).getName().toString();
+							}
+						}else{//**** block not executed in UNIT Test
+							if(innercallex instanceof CPPASTIdExpression){
+								CPPASTIdExpression a=(CPPASTIdExpression)innercallex.getFunctionNameExpression();
+								theName=a.getName().toString();
+							}else if(innercallex instanceof CPPASTFunctionCallExpression){
+								CPPASTFunctionCallExpression fce=(CPPASTFunctionCallExpression)innercallex;
+								IASTExpression expression=fce.getFunctionNameExpression();
+								if(expression instanceof CPPASTFieldReference){
+									CPPASTFieldReference a=(CPPASTFieldReference)expression;
+									theName=a.getFieldName().toString();	
+								}
+								if(expression instanceof CPPASTIdExpression){
+									CPPASTIdExpression a=(CPPASTIdExpression)expression;
+									theName=a.getName().toString();
+								}
+							}
+						}
+						
+						if(theName.equals(fname))return true;
+						}
+					
+				}catch(ClassCastException e){}
+			}	
+		}else{//TODO need to create suite
+			//@see getLastPushBack() for adding the very 1st push back
+		}
+		
+		return false;
+	}
 
 }
