c7ac319f70a07ccef854fe7a403ee0e2ba74bc80
james <james>
2008-04-14 12:16:03 +0000

    NEW - # 36: Adding PATH environment variable to run configuration of library test project http://r2.ifs.hsr.ch/cute/ticket/36   extract class

--- cute_plugin/src/ch/hsr/ifs/cutelauncher/CuteLaunchShortcut.java
+++ cute_plugin/src/ch/hsr/ifs/cutelauncher/CuteLaunchShortcut.java
@@ -15,29 +15,18 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
 
-import org.eclipse.cdt.core.envvar.IEnvironmentVariable;
 import org.eclipse.cdt.core.model.CModelException;
 import org.eclipse.cdt.core.model.CoreModel;
 import org.eclipse.cdt.core.model.IBinary;
 import org.eclipse.cdt.core.model.ICProject;
-import org.eclipse.cdt.core.settings.model.ICOutputEntry;
-import org.eclipse.cdt.core.settings.model.ICSourceEntry;
 import org.eclipse.cdt.debug.core.CDebugCorePlugin;
 import org.eclipse.cdt.debug.core.ICDTLaunchConfigurationConstants;
 import org.eclipse.cdt.debug.core.ICDebugConfiguration;
 import org.eclipse.cdt.launch.AbstractCLaunchDelegate;
 import org.eclipse.cdt.launch.internal.ui.LaunchMessages;
 import org.eclipse.cdt.launch.internal.ui.LaunchUIPlugin;
-import org.eclipse.cdt.managedbuilder.core.IConfiguration;
-import org.eclipse.cdt.managedbuilder.core.IManagedBuildInfo;
-import org.eclipse.cdt.managedbuilder.core.ManagedBuildManager;
-import org.eclipse.cdt.managedbuilder.envvar.IEnvironmentVariableProvider;
 import org.eclipse.cdt.ui.CElementLabelProvider;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IAdaptable;
@@ -189,107 +178,13 @@ public class CuteLaunchShortcut implements ILaunchShortcut {
 			
 			ICProject project=bin.getCProject();
 
-			String os = Platform.getOS();
-			if(os.equals(Platform.OS_WIN32))setWin32PATH(wc, project);
-			if(os.equals(Platform.OS_LINUX))setLinuxLD_LIBRARY_PATH(wc, project);
-			if(os.equals(Platform.OS_MACOSX))setMacDYLD_LIBRARY_PATH(wc, project);
-			
+			LaunchEnvironmentVariables.apply(wc,project);
+						
 			config = wc.doSave();
 		} catch (CoreException ce) {
 			CuteLauncherPlugin.log(ce);
 		}
 		return config;
-	}
-	
-	private void setMacDYLD_LIBRARY_PATH(ILaunchConfigurationWorkingCopy wc,
-			ICProject project) throws CoreException{
-		setEnvironmentVariable(wc,project,"DYLD_LIBRARY_PATH");
-	}
-	private void setLinuxLD_LIBRARY_PATH(ILaunchConfigurationWorkingCopy wc,
-			ICProject project) throws CoreException{
-		setEnvironmentVariable(wc,project,"LD_LIBRARY_PATH");
-	}
-	private void setWin32PATH(ILaunchConfigurationWorkingCopy wc,
-			ICProject project) throws CoreException{
-		setEnvironmentVariable(wc,project,"PATH");
-	}
-	private void setEnvironmentVariable(ILaunchConfigurationWorkingCopy wc,
-			ICProject project,String environmentVariableName) throws CoreException {
-		String path=getBuildEnvironmentVariable(environmentVariableName,project);
-		String pathSeparator=System.getProperty("path.separator");//assumption that it is only 1 char wide
-		if(!path.equals(""))
-			if( !(path.charAt(path.length()-1)+"").equals(pathSeparator))
-				path+=pathSeparator;
-		
-		IProject[] libProject=getReferencedProjects(project);
-		String libPath=generateLibPath(libProject,pathSeparator);
-
-		//Map m3=project.getOptions(true);//get information abt formatter etc
-//		Map m3=getBuildEnvironmentVariables(project);//get the entire build env variables
-		Map map=new TreeMap();
-		map.put(environmentVariableName, path+libPath);
-		wc.setAttribute(ILaunchManager.ATTR_ENVIRONMENT_VARIABLES, map);
-//		wc.setAttribute(ILaunchManager.ATTR_APPEND_ENVIRONMENT_VARIABLES, true);
-	}
-
-	private String getBuildEnvironmentVariable(String key,ICProject project) {
-		String result="";
-		IManagedBuildInfo info = ManagedBuildManager.getBuildInfo(project.getUnderlyingResource());
-		if (info != null) {
-			IConfiguration ic=info.getDefaultConfiguration();
-			IEnvironmentVariableProvider evp=ManagedBuildManager.getEnvironmentVariableProvider();
-			IEnvironmentVariable ev=evp.getVariable(key, ic, false);
-			result=ev.getValue();	
-		}
-		return result;
-	}
-
-	private IProject[] getReferencedProjects(ICProject project) throws CoreException {
-		IProject prj=project.getProject();
-		IProjectDescription desc = prj.getDescription();
-		IProject ref[]=desc.getReferencedProjects();
-		return ref;
-	}
-	
-	private String generateLibPath(IProject[] libProject, String pathSeparator) {
-		if(libProject.length<1)return "";
-		
-		String result="";
-		
-		for(int x=0;x<libProject.length;x++){
-			IManagedBuildInfo info = ManagedBuildManager.getBuildInfo(libProject[x]);
-			IConfiguration config = info.getDefaultConfiguration();
-			ICSourceEntry[] sources = config.getSourceEntries();
-			ICOutputEntry[]  dirs = config.getBuildData().getOutputDirectories();	
-			for (ICOutputEntry outputEntry : dirs) {
-				IPath location = outputEntry.getFullPath();
-				IPath parameter;
-				if(location.segmentCount()== 0){
-					parameter=libProject[x].getFullPath();
-				}else{
-					parameter=libProject[x].getFolder(location).getFullPath();	
-				}
-				result+= "${workspace_loc:" + parameter.toPortableString() + "}"+pathSeparator;
-			}	
-		}
-		
-		return result;
-	}
-	
-	private Map getBuildEnvironmentVariables(ICProject project) {
-		Map result=new TreeMap();
-		IManagedBuildInfo info = ManagedBuildManager.getBuildInfo(project.getUnderlyingResource());
-		if (info != null) {
-			IConfiguration ic=info.getDefaultConfiguration();
-			IEnvironmentVariableProvider evp=ManagedBuildManager.getEnvironmentVariableProvider();
-			IEnvironmentVariable[] ev=evp.getVariables(ic, false);
-			
-			for(IEnvironmentVariable iev:ev){
-				result.put(iev.getName(), iev.getValue());
-			}
-			
-		}
-		return result;
 	}
 	
 	/**
