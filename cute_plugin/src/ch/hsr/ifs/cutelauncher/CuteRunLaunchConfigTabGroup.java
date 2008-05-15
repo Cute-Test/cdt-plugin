78256cc58d45c143c74c0f21fce7ad45cfd2a445
james <james>
2008-05-15 09:55:44 +0000

    CLOSED - # 52: Refactoring CuteRunLaunchConfigTabGroup to remove max complexity method http://r2.ifs.hsr.ch/cute/ticket/52

--- cute_plugin/src/ch/hsr/ifs/cutelauncher/CuteRunLaunchConfigTabGroup.java
+++ cute_plugin/src/ch/hsr/ifs/cutelauncher/CuteRunLaunchConfigTabGroup.java
@@ -11,32 +11,18 @@
  ******************************************************************************/
 package ch.hsr.ifs.cutelauncher;
 
-import org.eclipse.cdt.core.CCorePlugin;
-import org.eclipse.cdt.core.ICDescriptor;
-import org.eclipse.cdt.core.model.CoreModel;
 import org.eclipse.cdt.core.model.ICElement;
-import org.eclipse.cdt.core.model.ICProject;
-import org.eclipse.cdt.debug.core.ICDTLaunchConfigurationConstants;
-import org.eclipse.cdt.launch.internal.ui.LaunchUIPlugin;
 import org.eclipse.cdt.launch.ui.CArgumentsTab;
+import org.eclipse.cdt.launch.ui.CLaunchConfigurationTab;
 import org.eclipse.cdt.launch.ui.CMainTab;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Platform;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationWorkingCopy;
 import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
 import org.eclipse.debug.ui.CommonTab;
 import org.eclipse.debug.ui.ILaunchConfigurationDialog;
 import org.eclipse.debug.ui.ILaunchConfigurationTab;
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.swt.widgets.Composite;
 
 import ch.hsr.ifs.cutelauncher.ui.CustomisedLaunchConfigTab;
 
@@ -61,7 +47,8 @@ public class CuteRunLaunchConfigTabGroup extends
 		
 		try{
 			ICElement cElement = null;
-			cElement = getContext(configuration, getPlatform(configuration));
+			proxyCLaunchConfigurationTab t=new proxyCLaunchConfigurationTab();
+			cElement = t.getContext1(configuration, t.getPlatform1(configuration));
 			if (cElement != null) {
 				cElement = cElement.getCProject();
 			
@@ -72,94 +59,24 @@ public class CuteRunLaunchConfigTabGroup extends
 		}
 		super.setDefaults(configuration);
 	}
-	///////////////////////////////////////////
-	// direct copy and paste from 
+
 	// @see org.eclipse.cdt.launch.ui.CLaunchConfigurationTab
 	// this is to hunt for current CProject
-	//////////////////////////////////////////
-	protected ICElement getContext(ILaunchConfiguration config, String platform) {
-		String projectName = null;
-		String programName = null;
-		IWorkbenchPage page = LaunchUIPlugin.getActivePage();
-		Object obj = null;
-		try {
-			projectName = config.getAttribute(ICDTLaunchConfigurationConstants.ATTR_PROJECT_NAME, (String)null);
-			programName = config.getAttribute(ICDTLaunchConfigurationConstants.ATTR_PROGRAM_NAME, (String)null);
-		} catch (CoreException e) {
+	class proxyCLaunchConfigurationTab extends CLaunchConfigurationTab{
+		public void performApply(ILaunchConfigurationWorkingCopy configuration){}
+		public void createControl(Composite parent){}
+		public void initializeFrom(ILaunchConfiguration configuration){}		
+		public String getName(){return "";}
+		public void setDefaults(ILaunchConfigurationWorkingCopy configuration){}
+		
+		public ICElement getContext1(ILaunchConfiguration config, String platform){
+			return this.getContext(config,platform);
 		}
-		if (projectName != null && !projectName.equals("")) { //$NON-NLS-1$
-			IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
-			ICProject cProject = CCorePlugin.getDefault().getCoreModel().create(project);
-			if (cProject != null && cProject.exists()) {
-				obj = cProject;
-			}
-		} else {
-			if (page != null) {
-				ISelection selection = page.getSelection();
-				if (selection instanceof IStructuredSelection) {
-					IStructuredSelection ss = (IStructuredSelection)selection;
-					if (!ss.isEmpty()) {
-						obj = ss.getFirstElement();
-					}
-				}
-			}
+		public String getPlatform1(ILaunchConfiguration config){
+			return this.getPlatform(config);
 		}
-		if (obj instanceof IResource) {
-			ICElement ce = CoreModel.getDefault().create((IResource)obj);
-			if (ce == null) {
-				IProject pro = ((IResource)obj).getProject();
-				ce = CoreModel.getDefault().create(pro);
-			}
-			obj = ce;
-		}
-		if (obj instanceof ICElement) {
-			if (platform != null && !platform.equals("*")) { //$NON-NLS-1$
-				ICDescriptor descriptor;
-				try {
-					descriptor = CCorePlugin.getDefault().getCProjectDescription( ((ICElement)obj).getCProject().getProject(),
-							false);
-					if (descriptor != null) {
-						String projectPlatform = descriptor.getPlatform();
-						if (!projectPlatform.equals(platform) && !projectPlatform.equals("*")) { //$NON-NLS-1$
-							obj = null;
-						}
-					}
-				} catch (CoreException e) {
-				}
-			}
-			if (obj != null) {
-				if (programName == null || programName.equals("")) { //$NON-NLS-1$
-					return (ICElement)obj;
-				}
-				ICElement ce = (ICElement)obj;
-				IProject project;
-				project = (IProject)ce.getCProject().getResource();
-				IPath programFile = project.getFile(programName).getLocation();
-				ce = CCorePlugin.getDefault().getCoreModel().create(programFile);
-				if (ce != null && ce.exists()) {
-					return ce;
-				}
-				return (ICElement)obj;
-			}
-		}
-		IEditorPart part = page.getActiveEditor();
-		if (part != null) {
-			IEditorInput input = part.getEditorInput();
-			return (ICElement)input.getAdapter(ICElement.class);
-		}
-		return null;
+		
 	}
-
-	protected String getPlatform(ILaunchConfiguration config) {
-		String platform = Platform.getOS();
-		try {
-			return config.getAttribute(ICDTLaunchConfigurationConstants.ATTR_PLATFORM, platform);
-		} catch (CoreException e) {
-			return platform;
-		}
-	}
-	///////////////////////////////////////////
-	
 	
 }
 //refer to CMainTab.initializeProgramName()
