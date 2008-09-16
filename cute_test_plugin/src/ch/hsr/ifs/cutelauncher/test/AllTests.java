e6802fe00097458526392199c8c0d780eaef21f7
Emanuel Graf <emanuel@graf.name>
2008-09-16 12:39:17 +0000

    build test

--- cute_test_plugin/src/ch/hsr/ifs/cutelauncher/test/AllTests.java
+++ cute_test_plugin/src/ch/hsr/ifs/cutelauncher/test/AllTests.java
@@ -19,13 +19,14 @@ public class AllTests {
 		suite.addTest(ModelBuilderSuite.suite());
 		suite.addTest(SourceLookupPathTest.suite());
 		suite.addTest(HyperlinkSuite.suite());
+		suite.addTest(CuteSuiteWizardHandlerTest.suite());
 		
 		Bundle b = org.eclipse.core.runtime.Platform.getBundle("org.eclipse.ui");
 		if ((b==null) || (b.getState() != Bundle.ACTIVE) || (! org.eclipse.ui.PlatformUI.isWorkbenchRunning())) {
 			//headless mode
 		}else{//requires UI
 			
-			suite.addTest(CuteSuiteWizardHandlerTest.suite());
+			
 			suite.addTest(SourceActionsTest.suite());
 		}
 		//$JUnit-END$
