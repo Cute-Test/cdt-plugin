abe28622f79a660ed3e46b1e13242249210d8f4d
Emanuel Graf <emanuel@graf.name>
2009-11-24 01:06:40 +0000

    init

---
+++ Cute_Header_1.6.0/suite/Test.cpp
@@ -0,0 +1,19 @@
+#include "cute.h"
+#include "ide_listener.h"
+#include "cute_runner.h"
+#include "$suitename$.h"
+
+//Version 1.6.0
+void runTest(){
+	cute::suite s=make_suite_$suitename$();
+	
+	cute::ide_listener lis;
+	cute::makeRunner(lis)(s, "The Suite");
+}
+
+int main(){
+    runTest();
+}
+
+
+
