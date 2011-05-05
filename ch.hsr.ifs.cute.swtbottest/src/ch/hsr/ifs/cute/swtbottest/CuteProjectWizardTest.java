38a7595ed460d74ac57b371fe8240c96756d00e6
Emanuel Graf <emanuel@graf.name>
2011-05-05 13:49:00 +0000

    *** empty log message ***

--- ch.hsr.ifs.cute.swtbottest/src/ch/hsr/ifs/cute/swtbottest/CuteProjectWizardTest.java
+++ ch.hsr.ifs.cute.swtbottest/src/ch/hsr/ifs/cute/swtbottest/CuteProjectWizardTest.java
@@ -10,17 +10,21 @@ package ch.hsr.ifs.cute.swtbottest;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import org.eclipse.cdt.core.CCorePlugin;
+import org.eclipse.cdt.core.settings.model.ICProjectDescription;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.swt.finder.junit.SWTBotJunit4ClassRunner;
 import org.eclipse.swtbot.swt.finder.waits.DefaultCondition;
 import org.eclipse.swtbot.swt.finder.waits.ICondition;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -35,6 +39,26 @@ import ch.hsr.ifs.cute.ui.project.headers.ICuteHeaders;
  */
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class CuteProjectWizardTest {
+
+	private final class JoinableMonitor extends NullProgressMonitor {
+		public boolean isDone = false;
+
+		@Override
+		public synchronized void done() {
+			isDone = true;
+			notifyAll();
+		}
+
+		public synchronized void join() {
+			while(!isDone) {
+				try {
+					wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
 	private final class ShellOpenCondition extends DefaultCondition {
 		private final SWTBotShell shell;
@@ -67,8 +91,8 @@ public class CuteProjectWizardTest {
 
 	@SuppressWarnings("nls")
 	@Test
-	public void openCuteTestWizard() throws CoreException {
-		String projectName = executeProjectWizard();
+	public void cuteProjectWizardHeaderIndex0() throws CoreException {
+		String projectName = executeProjectWizard(0, false, false);
 		IWorkspace workspace = ResourcesPlugin.getWorkspace();
 		IProject proj = workspace.getRoot().getProject(projectName);
 		assertNotNull("No Project", proj);
@@ -79,7 +103,32 @@ public class CuteProjectWizardTest {
 	}
 
 	@SuppressWarnings("nls")
-	private String executeProjectWizard() {
+	@Test
+	public void cuteProjectWizardHeaderIndex1() throws CoreException {
+		String projectName = executeProjectWizard(1, false, false);
+		IProject proj = ResourcesPlugin.getWorkspace().getRoot().getProject(projectName);
+		assertNotNull("No Project", proj);
+		IFolder cuteFolder =  proj.getFolder("cute");
+		assertNotNull(cuteFolder);
+		ICuteHeaders header = UiPlugin.getCuteVersionString(proj);
+		assertEquals(header.getVersionString(), cuteVersion);
+	}
+
+	@SuppressWarnings("nls")
+	@Test
+	public void cuteProjectWizardHeaderCopyBoost() throws CoreException {
+		String projectName = executeProjectWizard(0, true, false);
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IProject proj = workspace.getRoot().getProject(projectName);
+		assertNotNull("No Project", proj);
+		IFolder boost = proj.getFolder("boost");
+		assertNotNull(boost);
+		ICProjectDescription desc = CCorePlugin.getDefault().getProjectDescription(proj, false);
+		desc.getActiveConfiguration().getFolderDescriptions();
+	}
+
+	@SuppressWarnings("nls")
+	private String executeProjectWizard(int headerIndex, boolean copyBoost, boolean enableGcov) {
 		bot.perspectiveByLabel("C/C++").activate();
 		bot.menu("File").menu("New").menu("Project...").click();
 		final SWTBotShell shell = bot.shell("New Project");
@@ -90,19 +139,32 @@ public class CuteProjectWizardTest {
 		bot.textWithLabel("Project name:").setText(projectName);
 		SWTBotTree swtTree = bot.tree();
 		bot.checkBox("Show project types and toolchains only if they are supported on the platform").click();
-		//		sleep();
-		//		bot.list().select("Linux GCC");
-		//		sleep();
 		swtTree.select("Cute Project");
 		bot.button("Next >").click();
-		bot.comboBox().setSelection(0);
+		bot.comboBox().setSelection(headerIndex);
 		cuteVersion = bot.comboBox().selection();
-		bot.checkBox("Copy Boost headers into Project").click();
+		if(copyBoost) {
+			bot.checkBox("Copy Boost headers into Project").click();
+		}
+		if(enableGcov) {
+			bot.checkBox("Enable coverage analysis using gcov").click();
+		}
 		bot.button("Finish").click();
 		ICondition cond = new ShellOpenCondition(shell);
 		cond.init(bot);
 		bot.waitUntil(cond);
 		return projectName;
+	}
+
+	@After
+	public void cleanup() throws CoreException {
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		IProject[] projects = workspace.getRoot().getProjects();
+		for (IProject project : projects) {
+			JoinableMonitor monitor = new JoinableMonitor();
+			project.delete(true, monitor);
+			monitor.join();
+		}
 	}
 
 	@AfterClass
