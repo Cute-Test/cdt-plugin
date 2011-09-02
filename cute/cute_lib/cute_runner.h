d62054a9bce0a391c091063165af8d7f8ffe150f
Peter Sommerlad <peter.sommerlad@hsr.ch>
2011-09-02 16:46:23 +0000

    new runner filter feature -- tests still missing

--- cute/cute_lib/cute_runner.h
+++ cute/cute_lib/cute_runner.h
@@ -23,39 +23,129 @@
 #include "cute_test.h"
 #include "cute_suite.h"
 #include "cute_listener.h"
+#include <algorithm>
+#include <set>
 namespace cute {
-	template <typename Listener=null_listener>
-	struct runner : Listener{
-		runner():Listener(){}
-		runner(Listener &s):Listener(s){}
-		bool operator()(test const &t){
-			return runit(t);
-		}
-		bool operator()(suite const &s,char const *info=""){
-			Listener::begin(s,info);
-			bool result=true;
-			for(suite::const_iterator it=s.begin();
-			    it != s.end();
-			    ++it){
-			    	result = this->runit(*it) && result;
-			    }
-			Listener::end(s,info);
-			return result;
+	namespace runner_aux {
+	struct prefixMatcher// : std::unary_function<std::string const &,bool>
+	{
+		prefixMatcher(std::string const &prefix):prefix(prefix){}
+		bool operator()(std::string const &s) const {
+			size_t found=s.find(prefix);
+			return found==0 && (s.size()==prefix.size() || s[prefix.size()]=='#');
 		}
 	private:
-		bool runit(test const &t){
-			try {
-				Listener::start(t);
-				t();
-				Listener::success(t,"OK");
-				return true;
-			} catch (cute::test_failure const &e){
-				Listener::failure(t,e);
-			} catch (std::exception const &exc){
-				Listener::error(t,demangle(exc.what()).c_str());
-			} catch (std::string &s){
-				Listener::error(t,s.c_str());
-			} catch (char const *&cs) {
+		std::string const prefix;
+	};
+	struct prefixCutter //: std::unary_function<std::string, std::string>
+	{
+		prefixCutter(std::string const &prefix):prefix(prefix){}
+		std::string operator()(std::string s) const {
+			size_t found=s.find(prefix);
+			if ( found==0 && s.size()>prefix.size() && s[prefix.size()]=='#'){
+				s = s.substr(prefix.size()+1);
+			} else {
+				s.clear(); // either no match, or no individual test
+			}
+			return s;
+		}
+	private:
+		std::string const prefix;
+	};
+
+	struct ArgvTestFilter:std::unary_function<const test& ,bool>
+	{
+	    ArgvTestFilter(int argc, const char *const *const argv)
+	    :argc(argc), argv(argv)
+	    {
+	        if(needsFiltering()){
+	        	args.reserve(argc-1);
+	            std::copy(argv + 1, argv + argc,back_inserter(args));
+	        }
+	    }
+
+	    bool operator ()(const test & t) const
+	    {
+	        return !shouldRun(t.name());
+	    }
+
+	    bool shouldRun(const std::string & name) const
+	    {
+	        return match.empty() || match.count(name);
+	    }
+
+	    bool shouldRunSuite(std::string info)
+	    {
+	        match.clear();
+	        if(!needsFiltering() || !info.size())
+	            return true;
+	        if(args.end() != find_if(args.begin(), args.end(), prefixMatcher(info))){
+	           std::transform(args.begin(), args.end(), std::inserter(match,match.begin()),prefixCutter(info));
+	           match.erase(std::string()); // get rid of empty string
+	           return true;
+	        }
+	        return false;
+	    }
+
+	    bool needsFiltering() const
+	    {
+	        return argc > 1 && argv;
+	    }
+
+	private:
+	    std::set<std::string> match;
+	    std::vector<std::string> args;
+	    const int argc;
+	    const char * const * const argv;
+	};
+	} // namespace runner_aux
+	template <typename Listener=null_listener>
+	struct runner : Listener{
+
+		runner_aux::ArgvTestFilter filter;
+		runner():Listener(),filter(0,0){}
+		runner(Listener &s, int argc = 0, const char *const *argv = 0):Listener(s),filter(argc,argv){}
+		bool operator()(const test & t)
+	    {
+	        return runit(t);
+	    }
+
+	    bool operator ()(suite s, const char *info = "") // copy intentional for filtering support
+	    { // copy elision should happen with good compiler, because this will be inlined
+	        bool result = true;
+	        if(filter.shouldRunSuite(info)){ // side effect on filter
+	            filterSuite(s); // to make size correct in output, otherwise superfluous, but do not want to change Listener API
+	            Listener::begin(s, info);
+	            for(suite::const_iterator it = s.begin();it != s.end();++it){
+	                result = this->runit(*it) && result;
+	            }
+	            Listener::end(s, info);
+	        }
+
+	        return result;
+	    }
+	private:
+	    void filterSuite(suite & s)
+	    {
+	    	if (filter.needsFiltering() && s.end()!=find_if(s.begin(),s.end(),std::not1(filter))){
+	    		s.erase(std::remove_if(s.begin(),s.end(),filter),s.end());
+	    	}
+	    }
+
+	    bool runit(const test & t)
+	    {
+	        try {
+	            Listener::start(t);
+	            t();
+	            Listener::success(t, "OK");
+	            return true;
+	        } catch(const cute::test_failure & e){
+	            Listener::failure(t, e);
+	        } catch(const std::exception & exc){
+	            Listener::error(t, demangle(exc.what()).c_str());
+	        } catch(std::string & s){
+	            Listener::error(t, s.c_str());
+	        } catch(const char *&cs) {
 				Listener::error(t,cs);
 			} catch(...) {
 				Listener::error(t,"unknown exception thrown");
@@ -64,8 +154,8 @@ namespace cute {
 		}
 	};
 	template <typename Listener>
-	runner<Listener> makeRunner(Listener &s){
-		return runner<Listener>(s);
+	runner<Listener> makeRunner(Listener &s, int argc = 0, const char *const *argv = 0){
+		return runner<Listener>(s,argc,argv);
 	}
 }
 #endif /*CUTE_RUNNER_H_*/
