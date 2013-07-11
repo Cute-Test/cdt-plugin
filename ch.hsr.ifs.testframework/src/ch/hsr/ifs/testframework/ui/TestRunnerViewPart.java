7f96f66c9cda1526cccc00348b01995121220774
Peter Sommerlad <peter.sommerlad@hsr.ch>
2013-07-11 18:25:31 +0200

    fixed import

--- ch.hsr.ifs.testframework/src/ch/hsr/ifs/testframework/ui/TestRunnerViewPart.java
+++ ch.hsr.ifs.testframework/src/ch/hsr/ifs/testframework/ui/TestRunnerViewPart.java
@@ -29,6 +29,7 @@ import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ControlEvent;
 import org.eclipse.swt.events.ControlListener;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
