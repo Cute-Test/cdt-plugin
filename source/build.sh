68337bcce704164f152bb3d0f38b8ffd1d8b8eac
Michael Rüegg <rueegg.michael@gmail.com>
2014-03-29 11:09:22 +0100

    got rid of separate maven call in build script and use jenkins for this instead

--- source/build.sh
+++ source/build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 MOCKATOR_LIB_DIR=ch.hsr.ifs.mockator.lib
 MOCKATOR_PLUGIN_DIR=ch.hsr.ifs.mockator.plugin
-JVM_ARGS=$*
-export PATH=/usr/local/maven3/bin:$PATH
 
 init_path() {
   THIS=$(readlink -f $0)
@@ -28,9 +26,4 @@ copy_mockator_header_file() {
   cp -p $MOCKATOR_LIB_DIR/mockator/mockator.h $MOCKATOR_PLUGIN_DIR/headers/
 }
 
-build_plugin() {
-  mvn -e clean install -DJVM_ARGS="$JVM_ARGS"
-}
-
-init_path && mockator_unit_tests_for_cpp03 && mockator_unit_tests_for_cpp11 \
-&& copy_mockator_header_file && build_plugin
+init_path && mockator_unit_tests_for_cpp03 && mockator_unit_tests_for_cpp11 && copy_mockator_header_file
