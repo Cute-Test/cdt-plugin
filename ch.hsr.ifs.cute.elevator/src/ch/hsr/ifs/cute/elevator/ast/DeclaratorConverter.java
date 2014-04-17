8017c6eaf22ade94e5e1e7f1723436771c49525f
Silvano Brugnoni <sbrugnon@hsr.ch>
2014-04-17 16:32:26 +0200

    elevator: improved readabilty of declaration transformation code

--- ch.hsr.ifs.cute.elevator/src/ch/hsr/ifs/cute/elevator/ast/DeclaratorConverter.java
+++ ch.hsr.ifs.cute.elevator/src/ch/hsr/ifs/cute/elevator/ast/DeclaratorConverter.java
@@ -8,50 +8,103 @@ import org.eclipse.cdt.core.dom.ast.IASTInitializerList;
 import org.eclipse.cdt.core.dom.ast.IASTNode.CopyStyle;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTConstructorInitializer;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTFunctionCallExpression;
-import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTInitializerList;
 import org.eclipse.cdt.core.dom.ast.cpp.ICPPASTSimpleTypeConstructorExpression;
 
 /**
- * Converts a declarator to a C++11 initializer list.
+ * Converts declarators into C++11 initializer lists.
  * 
  */
 
 public class DeclaratorConverter {
     
     private final IASTDeclarator declarator;
+    private IASTDeclarator result;
 
     public DeclaratorConverter(IASTDeclarator declarator) {
         this.declarator = declarator;
     }
 
     public IASTDeclarator convert() {
-        IASTDeclarator convertedDeclarator = declarator.copy();
-        IASTInitializerList initList = declarator.getTranslationUnit().getASTNodeFactory().newInitializerList();
-        IASTInitializer convertedInitializer = convertedDeclarator.getInitializer();
-      
-        if (convertedInitializer instanceof IASTEqualsInitializer) {
-            IASTEqualsInitializer eqInitializer = (IASTEqualsInitializer) convertedInitializer;
+        final IASTInitializer initializer = declarator.getInitializer();
+        if (initializer instanceof IASTEqualsInitializer) {
+            IASTEqualsInitializer eqInitializer = (IASTEqualsInitializer) initializer;
             if (eqInitializer.getInitializerClause() instanceof ICPPASTFunctionCallExpression) {
-                for (IASTInitializerClause clause :  ((ICPPASTFunctionCallExpression) eqInitializer.getInitializerClause()).getArguments()) {
-                    initList.addClause(clause);
-                }
-            } else if (eqInitializer.getInitializerClause() instanceof ICPPASTSimpleTypeConstructorExpression){
-                IASTInitializer initializerList = ((ICPPASTSimpleTypeConstructorExpression) eqInitializer.getInitializerClause()).getInitializer();
-                if (initializerList instanceof IASTInitializerList) {
-                    initList = (IASTInitializerList) initializerList.copy(CopyStyle.withoutLocations);
-                }
+                transformConstructorCall(eqInitializer);
+            } else if (eqInitializer.getInitializerClause() instanceof ICPPASTSimpleTypeConstructorExpression) {
+                transformCopyConstructorWithUniformInitializedInitializer(eqInitializer);
             } else {
-                initList.addClause(((IASTEqualsInitializer) convertedInitializer).getInitializerClause());
+                transformCopyConstruction(eqInitializer);
             }
-        } else if (convertedInitializer instanceof ICPPASTConstructorInitializer) {
-            for (IASTInitializerClause clause : ((ICPPASTConstructorInitializer) convertedInitializer).getArguments()) {
-                initList.addClause(clause);
-            }
-        } else if (convertedInitializer instanceof ICPPASTInitializerList) {
-            initList = (ICPPASTInitializerList) convertedInitializer;
+        } else if (initializer instanceof ICPPASTConstructorInitializer) {
+            transformConstructorInitializer(initializer);
+        } else {
+            transformUninitializedDeclaration();
         }
-        convertedDeclarator.setInitializer(initList);
-        return convertedDeclarator;
+        return result;
     }
 
+    /**
+     * T t gets transformed to T t { }
+     */
+    private void transformUninitializedDeclaration() {
+        result = createDeclarator(createInitializerList());
+    }
+
+    /**
+     * T t(x) gets transformed to T t { x }
+     * @param initializer
+     */
+    private void transformConstructorInitializer(final IASTInitializer initializer) {
+        IASTInitializerList initializerList = createInitializerList(((ICPPASTConstructorInitializer) initializer).getArguments());
+        result = createDeclarator(initializerList);
+    }
+
+    /**
+     * T t = x gets transformed to T t { x }
+     * @param eqInitializer
+     */
+    private void transformCopyConstruction(IASTEqualsInitializer eqInitializer) {
+        IASTInitializerList initializerList = createInitializerList();
+        initializerList.addClause(eqInitializer.getInitializerClause().copy());
+        result = createDeclarator(initializerList);
+    }
+
+    /**
+     * T t = T { } gets transformed to T t { }
+     * @param eqInitializer
+     */
+    private void transformCopyConstructorWithUniformInitializedInitializer(IASTEqualsInitializer eqInitializer) {
+        IASTInitializer initializerList = ((ICPPASTSimpleTypeConstructorExpression) eqInitializer.getInitializerClause()).getInitializer();
+        if (initializerList instanceof IASTInitializerList) {
+            result = createDeclarator((IASTInitializerList) initializerList.copy(CopyStyle.withoutLocations));
+        }
+    }
+
+    /**
+     * T t = T() gets transformed to T t { }
+     * @param eqInitializer
+     */
+    private void transformConstructorCall(IASTEqualsInitializer eqInitializer) {
+        ICPPASTFunctionCallExpression constructorCall = (ICPPASTFunctionCallExpression) eqInitializer.getInitializerClause();
+        IASTInitializerList initializerList = createInitializerList(constructorCall.getArguments());
+        result = createDeclarator(initializerList);
+    }
+
+    private IASTInitializerList createInitializerList(IASTInitializerClause[] arguments) {
+        IASTInitializerList initializerList = createInitializerList();
+        for (IASTInitializerClause clause :   arguments) {
+            initializerList.addClause(clause.copy());
+        }
+        return initializerList;
+    }
+    
+    private IASTDeclarator createDeclarator(IASTInitializer initializer) {
+        IASTDeclarator convertedDeclarator = declarator.copy();
+        convertedDeclarator.setInitializer(initializer);
+        return convertedDeclarator;
+    }
+    
+    private IASTInitializerList createInitializerList() {
+       return declarator.getTranslationUnit().getASTNodeFactory().newInitializerList();
+    }
 }
