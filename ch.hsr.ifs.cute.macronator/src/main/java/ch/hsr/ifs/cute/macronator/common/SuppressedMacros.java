9b41e6f3ecf6cd1c3076d203cfd7a9e9374bb947
Thomas Corbat <tcorbat@hsr.ch>
2014-06-27 15:10:12 +0200

    Added proper resource closing for .suppressed file.

--- ch.hsr.ifs.cute.macronator/src/main/java/ch/hsr/ifs/cute/macronator/common/SuppressedMacros.java
+++ ch.hsr.ifs.cute.macronator/src/main/java/ch/hsr/ifs/cute/macronator/common/SuppressedMacros.java
@@ -16,58 +16,73 @@ import org.eclipse.cdt.codan.core.cxx.Activator;
 
 public class SuppressedMacros {
 
-    private Path file;
-    private Set<String> suppressedMacros;
+	private Path file;
+	private Set<String> suppressedMacros;
 
-    public SuppressedMacros(Path file) {
-        this.file = file;
-        this.suppressedMacros = new HashSet<String>();
-    }
+	public SuppressedMacros(Path file) {
+		this.file = file;
+		this.suppressedMacros = new HashSet<String>();
+	}
 
-    public void add(String macroName) {
-        if (macroName.length() > 0) {
-            suppressedMacros.add(macroName);
-        }
-        persistSuppressedMacros();
+	public void add(String macroName) {
+		if (macroName.length() > 0) {
+			suppressedMacros.add(macroName);
+		}
+		persistSuppressedMacros();
 
-    }
+	}
 
-    public void remove(String macroName) {
-        suppressedMacros.remove(macroName);
-        persistSuppressedMacros();
-    }
+	public void remove(String macroName) {
+		suppressedMacros.remove(macroName);
+		persistSuppressedMacros();
+	}
 
-    public boolean isSuppressed(String macroName) {
-        readSuppressedMacros();
-        return suppressedMacros.contains(macroName);
-    }
+	public boolean isSuppressed(String macroName) {
+		readSuppressedMacros();
+		return suppressedMacros.contains(macroName);
+	}
 
-    private void readSuppressedMacros() {
-        try {
-            if (!Files.exists(file)) {
-                file = Files.createFile(file);
-            }
-            BufferedReader reader = Files.newBufferedReader(file, StandardCharsets.UTF_8);
-            String line = null;
-            suppressedMacros = new HashSet<String>();
-            while ((line = reader.readLine()) != null) {
-                suppressedMacros.add(line);
-            }
-        } catch (IOException e) {
-            Activator.log("Error reading suppressed macros", e);
-        }
-    }
+	private void readSuppressedMacros() {
+		BufferedReader reader = null;
+		try {
+			if (!Files.exists(file)) {
+				file = Files.createFile(file);
+			}
+			reader = Files.newBufferedReader(file, StandardCharsets.UTF_8);
+			String line = null;
+			suppressedMacros = new HashSet<String>();
+			while ((line = reader.readLine()) != null) {
+				suppressedMacros.add(line);
+			}
+		} catch (IOException e) {
+			Activator.log("Error reading suppressed macros", e);
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
 
-    private void persistSuppressedMacros() {
-        try {
-            BufferedWriter writer = Files.newBufferedWriter(file, StandardCharsets.UTF_8, CREATE, TRUNCATE_EXISTING);
-            for (String macro : suppressedMacros) {
-                writer.write(macro + "\n");
-            }
-            writer.flush();
-            writer.close();
-        } catch (IOException e) {
-            Activator.log("Error persisting suppressed macros", e);
-        }
-    }
+	private void persistSuppressedMacros() {
+		BufferedWriter writer = null;
+		try {
+			writer = Files.newBufferedWriter(file, StandardCharsets.UTF_8, CREATE, TRUNCATE_EXISTING);
+			for (String macro : suppressedMacros) {
+				writer.write(macro + "\n");
+			}
+			writer.flush();
+		} catch (IOException e) {
+			Activator.log("Error persisting suppressed macros", e);
+		} finally {
+			try {
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
