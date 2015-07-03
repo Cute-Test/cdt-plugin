46e46e660a1e018a7e6109b180a787ce0907de23
Thomas Corbat <tcorbat@hsr.ch>
2015-07-03 11:32:46 +0200

    Removed redundant FileFinderVisitor in Gcov.

--- ch.hsr.ifs.cute.ui/src/ch/hsr/ifs/cute/ui/FileFinderVisitor.java
+++ ch.hsr.ifs.cute.ui/src/ch/hsr/ifs/cute/ui/FileFinderVisitor.java
@@ -1,3 +1,5 @@
+package ch.hsr.ifs.cute.ui;
+
 /*******************************************************************************
  * Copyright (c) 2007-2011, IFS Institute for Software, HSR Rapperswil,
  * Switzerland, http://ifs.hsr.ch
@@ -6,18 +8,23 @@
  * purpose without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
  ******************************************************************************/
-package ch.hsr.ifs.cute.ui;
+
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceVisitor;
 import org.eclipse.core.runtime.CoreException;
 
-class FileFinderVisitor implements IResourceVisitor {
+/**
+ * @author Emanuel Graf IFS
+ * @author Thomas Corbat IFS
+ * 
+ */
+public class FileFinderVisitor implements IResourceVisitor {
 	private final String sourceFileName;
 	private IFile file;
 
-	FileFinderVisitor(String FileName) {
+	public FileFinderVisitor(String FileName) {
 		sourceFileName = FileName;
 	}
 
@@ -26,7 +33,7 @@ class FileFinderVisitor implements IResourceVisitor {
 	}
 
 	public boolean visit(IResource resource) throws CoreException {
-		if (resource.getName().equals(sourceFileName)) {
+		if (resource.getName().endsWith(sourceFileName) && resource instanceof IFile) {
 			file = (IFile) resource;
 			return false;
 		} else {
