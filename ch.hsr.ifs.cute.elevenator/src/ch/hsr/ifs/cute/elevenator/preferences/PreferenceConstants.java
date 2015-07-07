0a739673b56a959c9175a3cc6c90dbfc010b4a5b
msyfrig@oldlaptop <msyfrig@hsr.ch>
2015-07-07 10:56:32 +0200

    elevenator: added a first (generated) version for the preference page to select the C++ version for the whole workspace.

---
+++ ch.hsr.ifs.cute.elevenator/src/ch/hsr/ifs/cute/elevenator/preferences/PreferenceConstants.java
@@ -0,0 +1,18 @@
+package ch.hsr.ifs.cute.elevenator.preferences;
+
+/**
+ * Constant definitions for plug-in preferences
+ */
+public class PreferenceConstants {
+
+	public static final String CPP_VERSION = "defaultCppVersionForWorkspace";
+
+	public static final String P_PATH = "pathPreference";
+
+	public static final String P_BOOLEAN = "booleanPreference";
+
+	public static final String P_CHOICE = "choicePreference";
+
+	public static final String P_STRING = "stringPreference";
+
+}
