d23334b6d25c4a004628e4e333ea06593503d916
MrHaribo <jonas_biedermann@hotmail.com>
2015-07-07 13:29:19 +0200

    Improved Extension Point scheme to support multiple language versions

--- ch.hsr.ifs.cute.elevenator/src/ch/hsr/ifs/cute/elevenator/EvaluateContributions.java
+++ ch.hsr.ifs.cute.elevenator/src/ch/hsr/ifs/cute/elevenator/EvaluateContributions.java
@@ -1,6 +1,10 @@
 package ch.hsr.ifs.cute.elevenator;
 
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.ISafeRunnable;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.core.runtime.SafeRunner;
 
 import ch.hsr.ifs.cute.elevenator.definition.IVersionModificationOperation;
@@ -9,22 +13,24 @@ public class EvaluateContributions {
 	private static final String IVERSIONMODIFICATOR_ID = "ch.hsr.ifs.cute.elevenator.versionmodification";
 
 	public static void evaluateAll() {
-		// IExtensionRegistry registry = null;
-		// IConfigurationElement[] config = registry.getConfigurationElementsFor(IVERSIONMODIFICATOR_ID);
-		// try {
-		// for (IConfigurationElement e : config) {
-		// System.out.println("Evaluating extension");
-		// final Object o = e.createExecutableExtension("class");
-		// if (o instanceof IVersionModificationOperation) {
-		// executeExtension(o);
-		// }
-		// }
-		// } catch (CoreException ex) {
-		// System.out.println(ex.getMessage());
-		// }
+
+		IExtensionRegistry registry = Platform.getExtensionRegistry();
+
+		IConfigurationElement[] config = registry.getConfigurationElementsFor(IVERSIONMODIFICATOR_ID);
+		try {
+			for (IConfigurationElement e : config) {
+				System.out.println("Evaluating extension");
+				final Object o = e.createExecutableExtension("class");
+				if (o instanceof IVersionModificationOperation) {
+					executeExtension(o);
+				}
+			}
+		} catch (CoreException ex) {
+			System.out.println(ex.getMessage());
+		}
 	}
 
-	private void executeExtension(final Object o) {
+	private static void executeExtension(final Object o) {
 		ISafeRunnable runnable = new ISafeRunnable() {
 			@Override
 			public void handleException(Throwable e) {
