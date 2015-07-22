771b831d9ee44105f267e12dda66a3536f9e4a1f
Toni Suter <tonisuter@me.com>
2015-07-22 15:39:57 +0200

    elevenator: persist changes of the ProblemProfile

--- ch.hsr.ifs.cute.elevenator/src/ch/hsr/ifs/cute/elevenator/definition/EnableCodanCheckers.java
+++ ch.hsr.ifs.cute.elevenator/src/ch/hsr/ifs/cute/elevenator/definition/EnableCodanCheckers.java
@@ -1,57 +1,55 @@
 package ch.hsr.ifs.cute.elevenator.definition;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.eclipse.cdt.codan.core.CodanCorePlugin;
 import org.eclipse.cdt.codan.core.CodanRuntime;
 import org.eclipse.cdt.codan.core.PreferenceConstants;
+import org.eclipse.cdt.codan.core.model.ICheckersRegistry;
 import org.eclipse.cdt.codan.core.model.IProblem;
 import org.eclipse.cdt.codan.core.model.IProblemProfile;
+import org.eclipse.cdt.codan.internal.core.CodanPreferencesLoader;
 import org.eclipse.cdt.codan.internal.core.model.CodanProblem;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.ProjectScope;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.jface.preference.IPersistentPreferenceStore;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
 
 import ch.hsr.ifs.cute.elevenator.Activator;
 
+@SuppressWarnings("restriction")
 public final class EnableCodanCheckers {
 	private EnableCodanCheckers() {
-
 	}
-
-	public static void enableProblem(IProject project, boolean enabled, String pid) {
-		IProblemProfile profile = CodanRuntime.getInstance().getCheckersRegistry()
-				.getResourceProfileWorkingCopy(project);
-		enableProblem(profile.getProblems(), enabled, pid);
-		CodanRuntime.getInstance().getCheckersRegistry().updateProfile(project, profile); // then set for the project
-	}
-
-	private static void enableProblem(IProblem[] problems, boolean enabled, String pid) {
-		for (int i = 0; i < problems.length; i++) {
-			IProblem p = problems[i];
-			if (p.getId().equals(pid)) {
-				((CodanProblem) p).setEnabled(enabled);
-				break;
-			}
-		}
-	}
-
+	
 	public static void enableProblems(IProject project, boolean enabled, String... ids) {
-		IProblemProfile profile = CodanRuntime.getInstance().getCheckersRegistry()
-				.getResourceProfileWorkingCopy(project);
-		IProblem[] problems = profile.getProblems();
-		for (int i = 0; i < problems.length; i++) {
-			IProblem p = problems[i];
-			for (int j = 0; j < ids.length; j++) {
-				String pid = ids[j];
-				if (p.getId().equals(pid)) {
-					((CodanProblem) p).setEnabled(enabled);
-					break;
-				}
+		List<String> problemIDs = Arrays.asList(ids);
+		ICheckersRegistry checkersRegistry = CodanRuntime.getInstance().getCheckersRegistry();
+		IProblemProfile profile = checkersRegistry.getResourceProfileWorkingCopy(project);
+		IPersistentPreferenceStore prefStore = new ScopedPreferenceStore(new ProjectScope(project), CodanCorePlugin.PLUGIN_ID);
+		CodanPreferencesLoader codanPreferencesLoader = new CodanPreferencesLoader();
+		codanPreferencesLoader.setInput(profile);
+		
+		for(IProblem problem : profile.getProblems()) {
+			String problemID = problem.getId();
+			if(problemIDs.contains(problemID)) {
+				((CodanProblem) problem).setEnabled(enabled);
+				
+				String property = codanPreferencesLoader.getProperty(problemID);
+				prefStore.setValue(problemID, property);
 			}
 		}
-		CodanRuntime.getInstance().getCheckersRegistry().updateProfile(project, profile); // then set for the project
+		checkersRegistry.updateProfile(project, profile);
+				
+		try {
+			prefStore.save();
+		} catch (IOException e) {
+			Status status = new Status(Status.ERROR, Activator.PLUGIN_ID, "Could not save codan preferences");
+			Activator.getDefault().getLog().log(status);
+		}
 	}
 
 	public static void setPreference_UseWorkspaceSettings(IProject project, boolean useWorkspaceSettings) {
