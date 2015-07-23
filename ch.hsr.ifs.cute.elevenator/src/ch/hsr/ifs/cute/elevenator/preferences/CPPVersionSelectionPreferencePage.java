c394c5bb8ac976c1236dc8269ec1bbbacce80bda
MrHaribo <jonas_biedermann@hotmail.com>
2015-07-23 15:53:43 +0200

    elevenator: Refactored Preference Page to use Same Tree as Wizard

--- ch.hsr.ifs.cute.elevenator/src/ch/hsr/ifs/cute/elevenator/preferences/CPPVersionSelectionPreferencePage.java
+++ ch.hsr.ifs.cute.elevenator/src/ch/hsr/ifs/cute/elevenator/preferences/CPPVersionSelectionPreferencePage.java
@@ -1,38 +1,29 @@
 package ch.hsr.ifs.cute.elevenator.preferences;
 
-import org.eclipse.cdt.managedbuilder.ui.wizards.MBSCustomPageManager;
 import org.eclipse.jface.preference.PreferencePage;
-import org.eclipse.jface.wizard.IWizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
-import org.eclipse.swt.widgets.Group;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
 
 import ch.hsr.ifs.cute.elevenator.Activator;
-import ch.hsr.ifs.cute.elevenator.CPPVersionCheckedTreeFieldEditor;
 import ch.hsr.ifs.cute.elevenator.definition.CPPVersion;
-import ch.hsr.ifs.cute.elevenator.view.SelectVersionWizardPage;
-import ch.hsr.ifs.cute.elevenator.view.TreeSelectionToolbar;
-import ch.hsr.ifs.cute.elevenator.view.TreeSelectionToolbar.ISelectionToolbarAction;
+import ch.hsr.ifs.cute.elevenator.view.DefaultVersionSelector;
+import ch.hsr.ifs.cute.elevenator.view.ModificationTree;
 import ch.hsr.ifs.cute.elevenator.view.VersionSelectionCombo;
 
-public class CPPVersionSelectionPreferencePage extends PreferencePage
-		implements IWorkbenchPreferencePage, ISelectionToolbarAction {
-
-	private VersionSelectionCombo versionCombo;
-	private CPPVersionCheckedTreeFieldEditor modificationTree;
-	private Button defaultVersionButton;
+public class CPPVersionSelectionPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
 
 	public static final String PAGE_ID = "ch.hsr.ifs.cute.elevenator.preferences.CPPVersionSelectionPreferencePage";
-	private Label selectedDefaultVersionLabel;
+
+	private VersionSelectionCombo versionCombo;
+	private ModificationTree modificationTree;
+	private DefaultVersionSelector defaultVersionSelector;
 
 	public CPPVersionSelectionPreferencePage() {
 		setPreferenceStore(Activator.getDefault().getPreferenceStore());
@@ -40,14 +31,8 @@ public class CPPVersionSelectionPreferencePage extends PreferencePage
 	}
 
 	@Override
-	public void init(IWorkbench workbench) {
-	}
-
-	@Override
 	protected Control createContents(Composite parent) {
 		Composite composite = new Composite(parent, SWT.NONE);
-
-		// composite.setLayoutData(new GridData(GridData.FILL_HORIZONTAL));
 		composite.setLayout(new GridLayout(2, false));
 
 		versionCombo = new VersionSelectionCombo(composite, "C++ Version", SWT.NONE);
@@ -56,97 +41,36 @@ public class CPPVersionSelectionPreferencePage extends PreferencePage
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				CPPVersion selectedVersion = versionCombo.getSelectedVersion();
-				modificationTree.changeVersion(selectedVersion);
-				updateDefaultVersionButton();
+				// modificationTree.changeVersion(selectedVersion);
+				defaultVersionSelector.updateDefaultVersionButton();
 			}
 		});
 
-		defaultVersionButton = new Button(composite, SWT.PUSH);
-		defaultVersionButton.setLayoutData(new GridData(SWT.RIGHT, SWT.TOP, true, false));
-		defaultVersionButton.addSelectionListener(new SelectionAdapter() {
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				getPreferenceStore().setValue(CPPVersionPreferenceConstants.ELEVENATOR_VERSION_DEFAULT,
-						versionCombo.getSelectedVersion().toString());
-				updateDefaultVersionButton();
-				updateDefaultVersionLabel();
-			}
+		defaultVersionSelector = new DefaultVersionSelector(composite, versionCombo, SWT.NONE);
+		defaultVersionSelector.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false));
 
-		});
-		updateDefaultVersionButton();
-
-		selectedDefaultVersionLabel = new Label(composite, SWT.NONE);
-		selectedDefaultVersionLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.TOP, true, false, 2, 1));
-		updateDefaultVersionLabel();
-
-		Group modificationsGroup = new Group(composite, SWT.NONE);
-		modificationsGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
-		modificationsGroup.setText("Settings:");
-		modificationsGroup.setLayout(new GridLayout(1, false));
-
-		String selectedVersionString = getPreferenceStore()
-				.getString(CPPVersionPreferenceConstants.ELEVENATOR_VERSION_DEFAULT);
-		CPPVersion selectedVersion = CPPVersion.valueOf(selectedVersionString);
-		modificationTree = new CPPVersionCheckedTreeFieldEditor(modificationsGroup, selectedVersion);
-		modificationTree.setPage(this);
-		modificationTree.setPreferenceStore(getPreferenceStore());
-		modificationTree.load();
-
-		TreeSelectionToolbar selectionToolbar = new TreeSelectionToolbar(modificationsGroup, this, SWT.NONE);
-		selectionToolbar.setLayoutData(new GridData(SWT.LEFT, SWT.TOP, true, false));
+		modificationTree = new ModificationTree(composite, SWT.NONE);
+		modificationTree.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
 
 		return composite;
 	}
 
-	private void updateDefaultVersionLabel() {
-		String versionString = selectedVersionString();
-		selectedDefaultVersionLabel.setText("Current Default Version is " + versionString);
-	}
+	@Override
+	public void init(IWorkbench workbench) {
+		// TODO Auto-generated method stub
 
-	private void updateDefaultVersionButton() {
-		String versionString = selectedVersionString();
-		defaultVersionButton.setText("Set " + versionString + " as Default Version");
-
-		String storedDefaultVersionString = getPreferenceStore()
-				.getString(CPPVersionPreferenceConstants.ELEVENATOR_VERSION_DEFAULT);
-		String selectedDefaultVersionString = versionCombo.getSelectedVersion().toString();
-		boolean versionMatches = storedDefaultVersionString.equals(selectedDefaultVersionString);
-		defaultVersionButton.setEnabled(!versionMatches);
-	}
-
-	private void updateWizardFromPreferences() {
-		try {
-			IWizardPage[] pages = MBSCustomPageManager.getCustomPages();
-			for (IWizardPage page : pages) {
-				if (page instanceof SelectVersionWizardPage) {
-					((SelectVersionWizardPage) page).refreshSettings();
-					break;
-				}
-			}
-		} catch (NullPointerException e) {
-			// The wizard is not open, do nothing
-		}
-	}
-
-	private String selectedVersionString() {
-		return versionCombo.getSelectedVersion().getVersionString();
 	}
 
 	@Override
 	protected void performDefaults() {
-		modificationTree.loadDefault();
+		// TODO Auto-generated method stub
 		super.performDefaults();
 	}
 
 	@Override
 	public boolean performOk() {
-		modificationTree.store();
-		updateWizardFromPreferences();
-		return true;
+		// TODO Auto-generated method stub
+		return super.performOk();
 	}
 
-	@Override
-	public void selectAll(boolean selected) {
-		modificationTree.selectAll(selected);
-	}
 }
