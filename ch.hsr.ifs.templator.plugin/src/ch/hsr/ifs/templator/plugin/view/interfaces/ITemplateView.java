e39a9a7e94e21ab5af01777e8bad951c7c8e8efa
pipeaesac <mario.meili@live.com>
2016-04-04 09:18:36 +0200

    remove duplicated class
    import issue

--- ch.hsr.ifs.templator.plugin/src/ch/hsr/ifs/templator/plugin/view/interfaces/ITemplateView.java
+++
@@ -1,7 +0,0 @@
-package ch.hsr.ifs.templator.plugin.view.interfaces;
-
-import ch.hsr.ifs.templator.plugin.viewdata.ViewData;
-
-public interface ITemplateView {
-	void setRootData(ViewData data);
-}
