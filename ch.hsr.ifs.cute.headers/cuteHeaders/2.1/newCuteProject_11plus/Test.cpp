7af2708429124981630930133fc6a330b1ee200e
pipeaesac <mario.meili@live.com>
2016-05-13 14:19:43 +0200

    adjustment for 11plus cpp files

--- ch.hsr.ifs.cute.headers/cuteHeaders/2.1/newCuteProject_11plus/Test.cpp
+++ ch.hsr.ifs.cute.headers/cuteHeaders/2.1/newCuteProject_11plus/Test.cpp
@@ -7,18 +7,15 @@ void thisIsATest() {
 	ASSERTM("start writing tests", false);	
 }
 
-void runAllTests(int argc, char const *argv[]) {
-	cute::suite {s};
+bool runAllTests(int argc, char const *argv[]) {
+	cute::suite s { };
 	s.push_back(CUTE(thisIsATest));
 	cute::xml_file_opener xmlfile(argc, argv);
-	cute::xml_listener<cute::ide_listener<> > lis(xmlfile.out);
-	cute::makeRunner(lis, argc, argv)(s, "AllTests");
+	cute::xml_listener<cute::ide_listener<>> lis(xmlfile.out);
+	const auto runner {cute::makeRunner(lis, argc, argv)};
+	return runner(s, "AllTests");
 }
 
 int main(int argc, char const *argv[]) {
-    runAllTests(argc, argv);
-    return 0;
+    return runAllTests(argc, argv) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
-
-
-
