2841580fa4eda7caa455973e7ba1b3144e7daf51
Silvano Brugnoni <silvano.brugnoni@gmail.com>
2016-06-02 15:56:51 +0200

    Macronator: introduced AST cache to improve performance

--- ch.hsr.ifs.cute.macronator/src/main/java/ch/hsr/ifs/cute/macronator/checker/FunctionLikeMacroChecker.java
+++ ch.hsr.ifs.cute.macronator/src/main/java/ch/hsr/ifs/cute/macronator/checker/FunctionLikeMacroChecker.java
@@ -1,9 +1,13 @@
 package ch.hsr.ifs.cute.macronator.checker;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.cdt.codan.core.cxx.model.AbstractIndexAstChecker;
 import org.eclipse.cdt.core.dom.ast.IASTPreprocessorFunctionStyleMacroDefinition;
 import org.eclipse.cdt.core.dom.ast.IASTPreprocessorMacroDefinition;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.model.ITranslationUnit;
 
 import ch.hsr.ifs.cute.macronator.common.MacroClassifier;
 import ch.hsr.ifs.cute.macronator.common.MacroProperties;
@@ -14,25 +18,27 @@ import ch.hsr.ifs.cute.macronator.transform.VoidFunctionTransformer;
 
 public class FunctionLikeMacroChecker extends AbstractIndexAstChecker {
 
-	public static final String PROBLEM_ID = "ch.hsr.ifs.macronator.plugin.ObsoleteFunctionLikeMacro";
+    public static final String PROBLEM_ID = "ch.hsr.ifs.macronator.plugin.ObsoleteFunctionLikeMacro";
 
-	@Override
-	public void processAst(final IASTTranslationUnit translationUnit) {
+    @Override
+    public void processAst(final IASTTranslationUnit ast) {
+        final Map<ITranslationUnit, IASTTranslationUnit> astCache = new HashMap<>();
+        astCache.put(ast.getOriginatingTranslationUnit(), ast);
+        for (final IASTPreprocessorMacroDefinition macro : ast.getMacroDefinitions()) {
+            final MacroClassifier classifier = new MacroClassifier(macro, astCache);
+            final MacroProperties properties = new MacroProperties(macro);
+            if (classifier.isFunctionLike() 
+                    && isTransformationValid((IASTPreprocessorFunctionStyleMacroDefinition) macro) 
+                    && classifier.areDependenciesValid() 
+                    && !properties.suggestionsSuppressed()) {
+                reportProblem(PROBLEM_ID, macro);
+            }
+        }
+    }
 
-		for (final IASTPreprocessorMacroDefinition macro : translationUnit.getMacroDefinitions()) {
-			final MacroClassifier classifier = new MacroClassifier(macro);
-			final MacroProperties properties = new MacroProperties(macro);
-			if (classifier.isFunctionLike() && classifier.areDependenciesValid()
-					&& isTransformationValid((IASTPreprocessorFunctionStyleMacroDefinition) macro)
-					&& !properties.suggestionsSuppressed()) {
-				reportProblem(PROBLEM_ID, macro);
-			}
-		}
-	}
-
-	private boolean isTransformationValid(final IASTPreprocessorFunctionStyleMacroDefinition macro) {
-		return isTransformableToFunction(macro) && !new DeclarationTransformer(macro).isValid();
-	}
+    private boolean isTransformationValid(final IASTPreprocessorFunctionStyleMacroDefinition macro) {
+        return isTransformableToFunction(macro) && !new DeclarationTransformer(macro).isValid();
+    }
 
     private boolean isTransformableToFunction(final IASTPreprocessorFunctionStyleMacroDefinition macro) {
         return new MacroTransformation(new AutoFunctionTransformer(macro)).isValid() || new MacroTransformation(new VoidFunctionTransformer(macro)).isValid();
