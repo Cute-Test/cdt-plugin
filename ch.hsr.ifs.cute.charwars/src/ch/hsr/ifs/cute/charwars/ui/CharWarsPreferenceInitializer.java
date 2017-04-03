7e89ac30bf0e712844387605317c2b6ff8f4f3ec
Felix Morgner <fmorgner@hsr.ch>
2017-04-03 10:07:00 +0200

    Update Target Platform Definition

--- ch.hsr.ifs.cute.charwars/src/ch/hsr/ifs/cute/charwars/ui/CharWarsPreferenceInitializer.java
+++ ch.hsr.ifs.cute.charwars/src/ch/hsr/ifs/cute/charwars/ui/CharWarsPreferenceInitializer.java
@@ -1,14 +1,11 @@
 package ch.hsr.ifs.cute.charwars.ui;
 
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
-import org.eclipse.jface.preference.IPreferenceStore;
-
-import ch.hsr.ifs.cute.charwars.CharWarsPlugin;
 
 public class CharWarsPreferenceInitializer extends AbstractPreferenceInitializer {
 	@Override
 	public void initializeDefaultPreferences() {
-		IPreferenceStore store = CharWarsPlugin.getDefault().getPreferenceStore();
+		//IPreferenceStore store = CharWarsPlugin.getDefault().getPreferenceStore();
 		//store.setDefault(PreferenceConstants.SHOW_WHITESPACES, false);
 	}
 }
