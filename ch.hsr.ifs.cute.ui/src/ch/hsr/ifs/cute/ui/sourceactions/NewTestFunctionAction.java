47d613e1fe2724cb283c0308f690ab68148158c9
Thomas Corbat <tcorbat@hsr.ch>
2017-07-10 10:24:24 +0200

    fix missing index lock

--- ch.hsr.ifs.cute.ui/src/ch/hsr/ifs/cute/ui/sourceactions/NewTestFunctionAction.java
+++ ch.hsr.ifs.cute.ui/src/ch/hsr/ifs/cute/ui/sourceactions/NewTestFunctionAction.java
@@ -9,6 +9,7 @@
 package ch.hsr.ifs.cute.ui.sourceactions;
 
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
+import org.eclipse.cdt.core.index.IIndex;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.IDocument;
@@ -16,6 +17,8 @@ import org.eclipse.jface.text.TextSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.text.edits.MultiTextEdit;
 import org.eclipse.text.edits.TextEdit;
+
+import ch.hsr.ifs.cute.ui.CuteUIPlugin;
 
 /**
  * @author Emanuel Graf
@@ -42,21 +45,28 @@ public class NewTestFunctionAction extends AbstractFunctionAction {
 			TextSelection selection = (TextSelection) sel;
 
 			IASTTranslationUnit astTu = getASTTranslationUnit(file);
-
-			SuitePushBackFinder suitPushBackFinder = new SuitePushBackFinder();
-			astTu.accept(suitPushBackFinder);
-			final AddNewTestStrategy newTestStrategy = new AddNewTestStrategy(doc, file, astTu, funcName, suitPushBackFinder, selection);
-
-			if (suitPushBackFinder.getSuiteNode() == null) {
-				int insertOffset = newTestStrategy.getInsertOffset(astTu, selection, doc);
-				final AddSuiteStrategy newSuiteStrategy = new AddSuiteStrategy(newTestStrategy, insertOffset);
-				final MultiTextEdit compositeEdit = new MultiTextEdit();
-				compositeEdit.addChild(newTestStrategy.createInsertTestFunctionEdit(insertOffset, doc, funcName));
-				TextEdit[] suiteChildren = newSuiteStrategy.getEdit().removeChildren();
-				compositeEdit.addChildren(suiteChildren);
-				return compositeEdit;
-			} else {
-				return newTestStrategy.getEdit();
+			IIndex index = astTu.getIndex();
+			try {
+				index.acquireReadLock();
+				SuitePushBackFinder suitPushBackFinder = new SuitePushBackFinder();
+				astTu.accept(suitPushBackFinder);
+				final AddNewTestStrategy newTestStrategy = new AddNewTestStrategy(doc, file, astTu, funcName, suitPushBackFinder, selection);
+	
+				if (suitPushBackFinder.getSuiteNode() == null) {
+					int insertOffset = newTestStrategy.getInsertOffset(astTu, selection, doc);
+					final AddSuiteStrategy newSuiteStrategy = new AddSuiteStrategy(newTestStrategy, insertOffset);
+					final MultiTextEdit compositeEdit = new MultiTextEdit();
+					compositeEdit.addChild(newTestStrategy.createInsertTestFunctionEdit(insertOffset, doc, funcName));
+					TextEdit[] suiteChildren = newSuiteStrategy.getEdit().removeChildren();
+					compositeEdit.addChildren(suiteChildren);
+					return compositeEdit;
+				} else {
+					return newTestStrategy.getEdit();
+				}
+			} catch (InterruptedException e) {
+				CuteUIPlugin.log(e);
+			} finally {
+				index.releaseReadLock();
 			}
 		}
 		return strategy.getEdit();
