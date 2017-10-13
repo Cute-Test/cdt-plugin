da130869a6892ee4ab896286bc1caee012db99d4
Hansruedi Patzen <hpatzen@hsr.ch>
2017-10-13 21:12:21 +0200

    Elevenator: Removed

--- ch.hsr.ifs.cute.elevenator/src/ch/hsr/ifs/cute/elevenator/definition/IVersionModificationOperation.java
+++
@@ -1,7 +0,0 @@
-package ch.hsr.ifs.cute.elevenator.definition;
-
-import org.eclipse.core.resources.IProject;
-
-public interface IVersionModificationOperation {
-	void perform(IProject project, CPPVersion selectedVersion, boolean enabled);
-}
