d1d1e6c3a9e406a60fc346e96393a1381bda9765
Stautob <tobias.stauber@hsr.ch>
2018-02-22 17:03:31 +0100

    Used constant

--- source/bundles/ch.hsr.ifs.mockator.plugin/src/ch/hsr/ifs/mockator/plugin/mockobject/support/MockatorIncludeInserter.java
+++ source/bundles/ch.hsr.ifs.mockator.plugin/src/ch/hsr/ifs/mockator/plugin/mockobject/support/MockatorIncludeInserter.java
@@ -3,6 +3,7 @@ package ch.hsr.ifs.mockator.plugin.mockobject.support;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.cdt.core.dom.rewrite.ASTRewrite;
 
+import ch.hsr.ifs.mockator.plugin.MockatorConstants;
 import ch.hsr.ifs.mockator.plugin.refsupport.includes.AstIncludeNode;
 
 
@@ -10,7 +11,6 @@ import ch.hsr.ifs.mockator.plugin.refsupport.includes.AstIncludeNode;
 // #include "mockator.h"
 public class MockatorIncludeInserter {
 
-   private static final String       MOCKATOR_HEADER = "mockator.h";
    private final IASTTranslationUnit ast;
 
    public MockatorIncludeInserter(final IASTTranslationUnit ast) {
@@ -18,7 +18,7 @@ public class MockatorIncludeInserter {
    }
 
    public void insertWith(final ASTRewrite rewriter) {
-      final AstIncludeNode include = new AstIncludeNode(MOCKATOR_HEADER);
+      final AstIncludeNode include = new AstIncludeNode(MockatorConstants.MOCKATOR_HEADER);
       include.insertInTu(ast, rewriter);
    }
 }
