2c2d0946773bb52a72d13240e61b9142608ac4a3
Stautob <tobias.stauber@hsr.ch>
2018-04-17 16:41:24 +0200

    Configuration SWTBot

---
+++ source/tests/ch.hsr.ifs.mockator.plugin.tests/src/ch/hsr/ifs/mockator/plugin/tests/base/PluginUITestSuiteBase.java
@@ -0,0 +1,18 @@
+package ch.hsr.ifs.mockator.plugin.tests.base;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import ch.hsr.ifs.mockator.plugin.tests.base.util.PathProposalUtilTest;
+
+
+@RunWith(Suite.class)
+//@formatter:off
+@SuiteClasses({
+   
+   /*Base*/
+   PathProposalUtilTest.class,
+   
+})
+public class PluginUITestSuiteBase {}
