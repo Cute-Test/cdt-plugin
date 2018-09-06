9522898d91f0aed3b84dd29b3827e11c0e1be783
Thomas Corbat <tcorbat@hsr.ch>
2018-09-06 10:23:21 +0200

    Headers: Fix template for new suite (>= C++11).
    Replaced brace-initialization with value-initialization to circumvent
    problem with non-explicit greedy constructor template of cute::test.

--- CUTEProject/bundles/ch.hsr.ifs.cute.headers/cuteHeaders/2.2.1/newCuteSuite_11plus/Test.cpp
+++ CUTEProject/bundles/ch.hsr.ifs.cute.headers/cuteHeaders/2.2.1/newCuteSuite_11plus/Test.cpp
@@ -8,7 +8,7 @@ bool runSuite(int argc, char const *argv[]) {
 	cute::xml_file_opener xmlfile(argc, argv);
 	cute::xml_listener<cute::ide_listener<>> lis(xmlfile.out);
 	auto runner = cute::makeRunner(lis, argc, argv);
-	cute::suite s { make_suite_$suitename$() };
+	cute::suite s = make_suite_$suitename$();
 	bool success = runner(s, "$suitename$");
 	return success;
 }
