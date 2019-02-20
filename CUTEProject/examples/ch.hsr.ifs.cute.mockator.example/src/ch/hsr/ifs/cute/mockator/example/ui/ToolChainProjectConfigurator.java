8780e5703dd4edfac56d654fa13a8c8566f3e764
Toni Suter <tonisuter@me.com>
2019-02-20 14:56:30 +0100

    Mockator: configure artifact extension based on platform

--- CUTEProject/examples/ch.hsr.ifs.cute.mockator.example/src/ch/hsr/ifs/cute/mockator/example/ui/ToolChainProjectConfigurator.java
+++ CUTEProject/examples/ch.hsr.ifs.cute.mockator.example/src/ch/hsr/ifs/cute/mockator/example/ui/ToolChainProjectConfigurator.java
@@ -4,12 +4,18 @@ import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.IProgressMonitor;
 
 import ch.hsr.ifs.iltis.core.core.ui.examples.IExampleProjectConfigurator;
-import ch.hsr.ifs.iltis.cpp.core.buildconfiguration.ToolChainConfigurator;
+
+import ch.hsr.ifs.iltis.cpp.core.buildconfiguration.BuildConfigurator;
+
 
 public class ToolChainProjectConfigurator implements IExampleProjectConfigurator {
-	@Override
-	public void configureProject(IProject project, IProgressMonitor monitor) {
-		ToolChainConfigurator toolChainConfigurator = new ToolChainConfigurator(project);
-		toolChainConfigurator.setDefaultToolChain();
-	}
+
+    @Override
+    public void configureProject(IProject project, IProgressMonitor monitor) {
+        BuildConfigurator buildConfigurator = new BuildConfigurator(project);
+        buildConfigurator.setDefaultToolChain();
+        if (buildConfigurator.isSharedLibraryProject()) {
+            buildConfigurator.setArtifactExtension(BuildConfigurator.getSharedLibraryExtensionForPlatform());
+        }
+    }
 }
